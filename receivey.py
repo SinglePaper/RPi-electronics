0d0d1dd75ce1da35f79b152915c36471f48fc21f
SinglePaper <stefanhasenoot@hotmail.com>
2021-04-16 21:56:01 +0200

    Included GPIO out

--- receivey.py
+++ receivey.py
@@ -5,10 +5,10 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7,GPIO.OUT)
-GPIO.setup(11,GPIO.OUT)
-GPIO.setup(13,GPIO.OUT)
-GPIO.setup(15,GPIO.OUT)
+GPIO.setup(7,GPIO.OUT)  # Left motor forward
+GPIO.setup(11,GPIO.OUT)  # Left motor backward
+GPIO.setup(13,GPIO.OUT)  # Right motor forward
+GPIO.setup(15,GPIO.OUT)  # Right motor backward
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("192.168.2.64", 5432))
@@ -20,10 +20,35 @@ while True:
         clientsocket, address = s.accept()
         print(f"Successful connection with address {address[0]}.")
         while True:
-            data = clientsocket.recv(1024)
+            data = clientsocket.recv(1)
             if not data:
                 break
             print(f"{address[0]}: " + data.decode("utf-8"))
+            if clientsocket.recv(1) == "n":
+                GPIO.output(7, False)
+                GPIO.output(11, False)
+                GPIO.output(13, False)
+                GPIO.output(15, False)
+            elif clientsocket.recv(1) == "f":
+                GPIO.output(7, True)
+                GPIO.output(11, False)
+                GPIO.output(13, True)
+                GPIO.output(15, False)
+            elif clientsocket.recv(1) == "b":
+                GPIO.output(7, False)
+                GPIO.output(11, True)
+                GPIO.output(13, False)
+                GPIO.output(15, True)
+            elif clientsocket.recv(1) == "l":
+                GPIO.output(7, False)
+                GPIO.output(11, False)
+                GPIO.output(13, True)
+                GPIO.output(15, False)
+            elif clientsocket.recv(1) == "r":
+                GPIO.output(7, True)
+                GPIO.output(11, False)
+                GPIO.output(13, False)
+                GPIO.output(15, False)
         else:
             continue
     except:
--- sendy.py
+++ sendy.py
@@ -1,14 +1,122 @@
 ### Remote control code (Client)
 
 import socket
-from inputs import get_key
+from time import sleep
+from inputs import get_gamepad
+import math
+import threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.2.64", 5432))
-# s.send(b'Message from client to server.')
+class XboxController(object):
+    MAX_TRIG_VAL = math.pow(2, 8)
+    MAX_JOY_VAL = math.pow(2, 15)
 
-while True:
-    events = get_key()
-    for event in events:
-        s.send(encode(event.ev_type), "utf-8")
-s.close()
+    def __init__(self):
+
+        self.LeftJoystickY = 0
+        self.LeftJoystickX = 0
+        self.RightJoystickY = 0
+        self.RightJoystickX = 0
+        self.LeftTrigger = 0
+        self.RightTrigger = 0
+        self.LeftBumper = 0
+        self.RightBumper = 0
+        self.A = 0
+        self.X = 0
+        self.Y = 0
+        self.B = 0
+        self.LeftThumb = 0
+        self.RightThumb = 0
+        self.Back = 0
+        self.Start = 0
+        self.LeftDPad = 0
+        self.RightDPad = 0
+        self.UpDPad = 0
+        self.DownDPad = 0
+
+        self._monitor_thread = threading.Thread(target=self._monitor_controller, args=())
+        self._monitor_thread.daemon = True
+        self._monitor_thread.start()
+
+
+    def read(self): # return the buttons/triggers that you care about in this methode
+        x = self.LeftJoystickY
+        y = self.LeftJoystickX
+        A = self.A
+        return [x, y, A]
+
+
+    def _monitor_controller(self):
+        while True:
+            events = get_gamepad()
+            for event in events:
+                if event.code == 'ABS_Y':
+                    self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_X':
+                    self.LeftJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_RY':
+                    self.RightJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_RX':
+                    self.RightJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_Z':
+                    self.LeftTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                elif event.code == 'ABS_RZ':
+                    self.RightTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                elif event.code == 'BTN_TL':
+                    self.LeftBumper = event.state
+                elif event.code == 'BTN_TR':
+                    self.RightBumper = event.state
+                elif event.code == 'BTN_SOUTH':
+                    self.A = event.state
+                elif event.code == 'BTN_NORTH':
+                    self.X = event.state
+                elif event.code == 'BTN_WEST':
+                    self.Y = event.state
+                elif event.code == 'BTN_EAST':
+                    self.B = event.state
+                elif event.code == 'BTN_THUMBL':
+                    self.LeftThumb = event.state
+                elif event.code == 'BTN_THUMBR':
+                    self.RightThumb = event.state
+                elif event.code == 'BTN_SELECT':
+                    self.Back = event.state
+                elif event.code == 'BTN_START':
+                    self.Start = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY1':
+                    self.LeftDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY2':
+                    self.RightDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY3':
+                    self.UpDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY4':
+                    self.DownDPad = event.state
+
+def get_direction(data):
+    x = data[0]
+    y = data[1]
+    if abs(x) > abs(y) and x < -0.5:  # backwards
+        return "b"
+    elif abs(x) > abs(y) and x > 0.5:  # forwards
+        return "f"
+    elif abs(x) < abs(y) and y < -0.5:  # left
+        return "l"
+    elif abs(x) < abs(y) and y > 0.5:  # right
+        return "r"
+    else:
+        return "n"  # n for None (stops it from crashing when turning)
+
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("192.168.2.64", 5432))
+    # s.send(b'Message from client to server.')
+    controller = XboxController()
+    while True:
+        #try:
+            data = controller.read()
+            s.send(get_direction(data[:2]).encode("utf-8"))
+            sleep(.05)
+        #except:
+    #s.close()
+
+if __name__ == '__main__':
+    main()
