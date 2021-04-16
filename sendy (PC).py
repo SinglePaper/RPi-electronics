5512abf6f9f421785764d1eb880262ca271db558
SinglePaper <stefanhasenoot@hotmail.com>
2021-04-16 17:48:35 +0200

    Updated

--- sendy (PC).py
+++ sendy (PC).py
@@ -1,7 +1,7 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("127.0.0.1", 1234))
+s.connect(("192.168.2.18", 1234))
 
 msg = s.recv(1024)
 print(msg.decode("utf-8"))
