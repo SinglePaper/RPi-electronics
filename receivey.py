33eedb3bae1728b163cd3c7ac7426db54cb2e5bf
SinglePaper <stefanhasenoot@hotmail.com>
2021-04-16 18:00:04 +0200

    Oopsie

--- receivey.py
+++ receivey.py
@@ -1,11 +1,14 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("192.168.2.18", 1234))
+s.bind(("192.168.2.18", 5432))
 s.listen(5)
 
 while True:
-    print("Searching for a connection...")
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Connected to the server successfully", "utf-8"))
+    try:
+        print("Searching for a connection...")
+        clientsocket, address = s.accept()
+        print(f"Connection from {address} has been established!")
+        clientsocket.send(bytes("Connected to the server successfully", "utf-8"))
+    except:
+        clientsocket.close()
--- sendy (PC).py
+++ sendy (PC).py
@@ -1,7 +1,8 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.2.64", 1234))
+s.connect(("192.168.2.64", 5432))
 
 msg = s.recv(1024)
 print(msg.decode("utf-8"))
+s.close()
