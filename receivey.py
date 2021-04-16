d0b8a5716cf30a8e39fb6127e3e4f6f21fa565ad
SinglePaper <stefanhasenoot@hotmail.com>
2021-04-16 18:24:07 +0200

    Updated

--- receivey.py
+++ receivey.py
@@ -1,4 +1,14 @@
+# Raspberry Pi code ()
+
 import socket
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
+GPIO.setup(15,GPIO.OUT)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("192.168.2.64", 5432))
@@ -8,7 +18,8 @@ while True:
     try:
         print("Searching for a connection...")
         clientsocket, address = s.accept()
-        print(f"Connection from {address} has been established!")
+        print(f"{address}: ")
         clientsocket.send(bytes("Connected to the server successfully", "utf-8"))
     except:
         clientsocket.close()
+        break
--- sendy.py
+++ sendy.py
@@ -1,8 +1,11 @@
+# Remote control code (Client)
+
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("192.168.2.64", 5432))
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+while True:
+    msg = s.recv(1024)
+    print(msg.decode("utf-8"))
 s.close()
