90c8c654afab1b3fcd4ef6eb6f10291418805218
SinglePaper <stefanhasenoot@hotmail.com>
2021-04-19 16:42:55 +0200

    Adding support for web control

--- receivey.py
+++ receivey.py
@@ -10,6 +10,7 @@ GPIO.setup(11,GPIO.OUT)  # Left motor backward
 GPIO.setup(13,GPIO.OUT)  # Right motor forward
 GPIO.setup(15,GPIO.OUT)  # Right motor backward
 
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("192.168.2.64", 5432))
 s.listen(5)
