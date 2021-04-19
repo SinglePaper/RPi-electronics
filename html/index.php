390e4ab01a06e38ec58b82982590b95adf341e99
SinglePaper <stefanhasenoot@hotmail.com>
2021-04-19 18:15:30 +0200

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,15 +1,5 @@
 <?php
-$message = "It works!";
-?>
+$message = "It works!"
 
-<html>
-    <head>
-        <title>Charlie Control</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <h1>
-            <?=$message?>
-        </h1>
-    </body>
-</html>
+readfile("##home.html");
+?>
