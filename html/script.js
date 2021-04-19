0da70e9720c54d56cbb39def91abce2d1afef90f
SinglePaper <stefanhasenoot@hotmail.com>
2021-04-19 20:41:32 +0200

    Update

---
+++ html/script.js
@@ -0,0 +1,66 @@
+direction = 0
+
+up_button = document.querySelector("#up");
+down_button = document.querySelector("#down");
+left_button = document.querySelector("#left");
+right_button = document.querySelector("#right");
+
+// Events for up button
+up_button.addEventListener("mousedown", upPress, false);
+up_button.addEventListener("mouseup", resetPress, false);
+up_button.addEventListener("mouseleave", resetPress, false);
+
+// Events for down button
+down_button.addEventListener("mousedown", downPress, false);
+down_button.addEventListener("mouseup", resetPress, false);
+down_button.addEventListener("mouseleave", resetPress, false);
+
+// Events for left button
+left_button.addEventListener("mousedown", leftPress, false);
+left_button.addEventListener("mouseup", resetPress, false);
+left_button.addEventListener("mouseleave", resetPress, false);
+
+// Events for right button
+right_button.addEventListener("mousedown", rightPress, false);
+right_button.addEventListener("mouseup", resetPress, false);
+right_button.addEventListener("mouseleave", resetPress, false);
+
+function upPress(e) {
+    e.preventDefault();
+    console.log("Button: Up")
+    direction = 0                       // Maybe add support for making this a speed up button
+}
+function downPress(e) {
+    e.preventDefault();
+    console.log("Button: Down")
+    direction = 2
+}
+function leftPress(e) {
+    e.preventDefault();
+    console.log("Button: Left")
+    direction = 1
+}
+function rightPress(e) {
+    e.preventDefault();
+    console.log("Button: Right")
+    direction = 3
+}
+
+function resetPress(e) {
+    e.preventDefault();
+    console.log("Button released.")
+    direction = 0
+}
+
+while (true) {
+    switch (direction) {
+        case 0:
+            console.log("Forward")
+        case 1:
+            console.log("Left")
+        case 2:
+            console.log("Backward")
+        case 3:
+            console.log("Right")
+    }
+}
