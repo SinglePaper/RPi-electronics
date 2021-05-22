3231e4a4d46b9f43389232c4101072f454f03707
SinglePaper <stefanhasenoot@hotmail.com>
2021-05-22 16:07:42 +0200

    Update camera_pi.py

--- camera_pi.py
+++ camera_pi.py
@@ -36,7 +36,7 @@ class Camera(object):
         with picamera.PiCamera() as camera:
             # camera setup
             camera.resolution = (320, 240)
-            camera.framerate = 12
+            camera.framerate = 6
             camera.hflip = False
             camera.vflip = True
 
